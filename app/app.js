cb27d073cd53fe80eda42dae1660ce65bafb2031
mohammadawaisali <alim60@roehampton.ac.uk>
2025-03-23 16:23:09 +0000

    More handsome

--- app/app.js
+++ app/app.js
@@ -1,5 +1,9 @@
 // Import express.js
 const express = require("express");
+// Get the functions in the db.js file to use
+const db = require('./services/db');
+// At the top of app.js, require 'path' for safer path handling
+const path = require("path");
 
 // Create express app
 var app = express();
@@ -7,39 +11,23 @@ var app = express();
 // Add static files location
 app.use(express.static("static"));
 
-// Get the functions in the db.js file to use
-const db = require('./services/db');
-
 // Create a route for root - /
 app.get("/", function(req, res) {
-    res.send("Hello world!");
+  // __dirname is the absolute path to the folder containing this file (app.js).
+  // We move one level up to ../, then into frontend/, and serve the HTML file.
+  res.sendFile(path.join(__dirname, "..", "frontend", "main.html"));
 });
-
-// Create a route for testing the db
-app.get("/db_test", function(req, res) {
-    // Assumes a table called test_table exists in your database
-    sql = 'select * from test_table';
-    db.query(sql).then(results => {
-        console.log(results);
-        res.send(results)
-    });
+// Dynamic route for the "Donate Food" page
+app.get("/donate", function(req, res) {
+    res.sendFile(path.join(__dirname, "..", "frontend", "donor.html"));
+  });
+//Dynamic route for the "Receieve Food" page
+app.get("/receive", function(req, res) {
+    res.sendFile(path.join (__dirname, "..", "frontend", "recepient.html"));
 });
-
-// Create a route for /goodbye
-// Responds to a 'GET' request
-app.get("/goodbye", function(req, res) {
-    res.send("Goodbye world!");
-});
-
-// Create a dynamic route for /hello/<name>, where name is any value provided by user
-// At the end of the URL
-// Responds to a 'GET' request
-app.get("/hello/:name", function(req, res) {
-    // req.params contains any parameters in the request
-    // We can examine it in the console for debugging purposes
-    console.log(req.params);
-    //  Retrieve the 'name' parameter and use it in a dynamically generated page
-    res.send("Hello " + req.params.name);
+//Dynamic route for the "volunteer" page
+app.get("/volunteer", function(req, res) {
+    res.sendFile(path.join (__dirname, "..", "frontend", "volunteer.html"));
 });
 
 // Start server on port 3000
