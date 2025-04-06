eebaad6058a3dc3d3ee9ff81cf65edf7baaf13e9
ASYA-ANGELOVA <196176590+ASYA-ANGELOVA@users.noreply.github.com>
2025-04-06 15:15:22 +0100

    Add files via upload

---
+++ SPRINT_4_AMENDMENTS/community_fridges.php
@@ -0,0 +1,7 @@
+CREATE TABLE community_fridges(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    latitude DECIMAL(10,8) NOT NULL,
+    longitude DECIMAL(11,8) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
